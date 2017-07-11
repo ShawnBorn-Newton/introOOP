0869e07c47897085f69c49327a914074b1fa7f64
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-11 10:48:14 -0400

    practise3

--- introOOP/Cat.cs
+++ introOOP/Cat.cs
@@ -11,5 +11,34 @@ namespace introOOP
         //Inside the class, we creat the blue print for an object.
         //Classes are templets used to creat objects
         //should always be in Pascel case.
+
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungery;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+        }
+    public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+        public void Eat()
+            {
+            if(isHungery)
+            {
+                isHungery = false;
+            }
+            Console.WriteLine("Is the cat hungery?");
+        }
     }
 }
